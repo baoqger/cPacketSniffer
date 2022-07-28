a6d7198d5244c6f962e7ddebd716ffcaf67d4cc0
chris bao <baoqger@aliyun.com>
2022-07-28 15:10:24 +0800

    add function

--- sniffer/icmppacket.c
+++ sniffer/icmppacket.c
@@ -28,7 +28,25 @@ unsigned int checksum(icmppacket *i) {
     return char2word(i->p_data + 2);
 }
 
+// Returns content of identifier header field for type 13, 14, 17 or 18 ICMP packets
+unsigned int identifier(icmppacket *i) {
+    if (i->code(i) == 0 || (i->type(i) == 13 || i->type(i) == 14 || i->type(i) == 17 || i->type(i) == 18)) {
+        return char2word(i->p_data + 4);
+    } else {
+        fprintf(stderr, "ICMP packet does not hold identifier field\n");
+        exit(EXIT_FAILURE); 
+    }
+}
 
+// Returns content of sequence number header field for 13, 14, 17 or 18 ICMP packets
+unsigned int sequence_number(icmppacket *i) {
+    if(i->code(i) == 0 || (i->type(i) == 13 || i->type(i) == 14 || i->type(i) == 17 || i->type(i) == 18)) {
+        return char2word(i->p_data + 6);
+    } else {
+        fprintf(stderr, "ICMP packet does not hold sequence number field.\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 icmppacket* new_icmppacket(bool owned, unsigned char *p_data, unsigned int p_len) {
     icmppacket *i = malloc(sizeof(icmppacket));
@@ -37,6 +55,8 @@ icmppacket* new_icmppacket(bool owned, unsigned char *p_data, unsigned int p_len
     i->header_length = header_length;
     i->type = type;
     i->checksum = checksum;
+    i->identifier = identifier;
+    i->sequence_number = sequence_number;
     if(i->owned) { // copy data into a new block
         memcpy(i->p_data, p_data, p_len);
     } else {
